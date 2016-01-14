4c8df45586bc163dbf06fc0e0a4b31f9624e2ff3
DanielsCoding <daniel.sollner@it-informatik.de>
2016-01-14 13:48:55 +0100

    Inital
    Initaler Checkin

---
+++ AsciiBinHex/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AsciiBinHex
+{
+    partial class UmrechnerAsciiHExBin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAscii = new System.Windows.Forms.TextBox();
+            this.textBoxHex = new System.Windows.Forms.TextBox();
+            this.textBoxBin = new System.Windows.Forms.TextBox();
+            this.labelAscii = new System.Windows.Forms.Label();
+            this.labelHex = new System.Windows.Forms.Label();
+            this.labelBin = new System.Windows.Forms.Label();
+            this.textBoxUTF8 = new System.Windows.Forms.TextBox();
+            this.labelUTF8 = new System.Windows.Forms.Label();
+            this.labelBin2 = new System.Windows.Forms.Label();
+            this.labelHex2 = new System.Windows.Forms.Label();
+            this.textBoxBin2 = new System.Windows.Forms.TextBox();
+            this.textBoxHex2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxAscii
+            // 
+            this.textBoxAscii.Location = new System.Drawing.Point(47, 12);
+            this.textBoxAscii.Multiline = true;
+            this.textBoxAscii.Name = "textBoxAscii";
+            this.textBoxAscii.Size = new System.Drawing.Size(204, 103);
+            this.textBoxAscii.TabIndex = 0;
+            this.textBoxAscii.TextChanged += new System.EventHandler(this.textBoxAscii_TextChanged);
+            this.textBoxAscii.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxAscii_KeyDown);
+            // 
+            // textBoxHex
+            // 
+            this.textBoxHex.Location = new System.Drawing.Point(47, 121);
+            this.textBoxHex.Name = "textBoxHex";
+            this.textBoxHex.Size = new System.Drawing.Size(204, 20);
+            this.textBoxHex.TabIndex = 1;
+            this.textBoxHex.TextChanged += new System.EventHandler(this.textBoxHex_TextChanged);
+            this.textBoxHex.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxHex_KeyDown);
+            // 
+            // textBoxBin
+            // 
+            this.textBoxBin.Location = new System.Drawing.Point(47, 147);
+            this.textBoxBin.Multiline = true;
+            this.textBoxBin.Name = "textBoxBin";
+            this.textBoxBin.Size = new System.Drawing.Size(204, 36);
+            this.textBoxBin.TabIndex = 2;
+            this.textBoxBin.TextChanged += new System.EventHandler(this.textBoxBin_TextChanged);
+            this.textBoxBin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxBin_KeyDown);
+            // 
+            // labelAscii
+            // 
+            this.labelAscii.AutoSize = true;
+            this.labelAscii.Location = new System.Drawing.Point(9, 15);
+            this.labelAscii.Name = "labelAscii";
+            this.labelAscii.Size = new System.Drawing.Size(32, 13);
+            this.labelAscii.TabIndex = 3;
+            this.labelAscii.Text = "Ascii:";
+            // 
+            // labelHex
+            // 
+            this.labelHex.AutoSize = true;
+            this.labelHex.Location = new System.Drawing.Point(9, 124);
+            this.labelHex.Name = "labelHex";
+            this.labelHex.Size = new System.Drawing.Size(29, 13);
+            this.labelHex.TabIndex = 4;
+            this.labelHex.Text = "Hex:";
+            // 
+            // labelBin
+            // 
+            this.labelBin.AutoSize = true;
+            this.labelBin.Location = new System.Drawing.Point(9, 150);
+            this.labelBin.Name = "labelBin";
+            this.labelBin.Size = new System.Drawing.Size(25, 13);
+            this.labelBin.TabIndex = 5;
+            this.labelBin.Text = "Bin:";
+            // 
+            // textBoxUTF8
+            // 
+            this.textBoxUTF8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUTF8.Location = new System.Drawing.Point(315, 12);
+            this.textBoxUTF8.Multiline = true;
+            this.textBoxUTF8.Name = "textBoxUTF8";
+            this.textBoxUTF8.ReadOnly = true;
+            this.textBoxUTF8.Size = new System.Drawing.Size(191, 103);
+            this.textBoxUTF8.TabIndex = 6;
+            this.textBoxUTF8.TextChanged += new System.EventHandler(this.textBoxUTF8_TextChanged);
+            this.textBoxUTF8.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUTF8_KeyDown);
+            // 
+            // labelUTF8
+            // 
+            this.labelUTF8.AutoSize = true;
+            this.labelUTF8.Location = new System.Drawing.Point(269, 15);
+            this.labelUTF8.Name = "labelUTF8";
+            this.labelUTF8.Size = new System.Drawing.Size(40, 13);
+            this.labelUTF8.TabIndex = 7;
+            this.labelUTF8.Text = "UTF-8:";
+            // 
+            // labelBin2
+            // 
+            this.labelBin2.AutoSize = true;
+            this.labelBin2.Location = new System.Drawing.Point(269, 150);
+            this.labelBin2.Name = "labelBin2";
+            this.labelBin2.Size = new System.Drawing.Size(25, 13);
+            this.labelBin2.TabIndex = 11;
+            this.labelBin2.Text = "Bin:";
+            // 
+            // labelHex2
+            // 
+            this.labelHex2.AutoSize = true;
+            this.labelHex2.Location = new System.Drawing.Point(269, 124);
+            this.labelHex2.Name = "labelHex2";
+            this.labelHex2.Size = new System.Drawing.Size(29, 13);
+            this.labelHex2.TabIndex = 10;
+            this.labelHex2.Text = "Hex:";
+            // 
+            // textBoxBin2
+            // 
+            this.textBoxBin2.Location = new System.Drawing.Point(315, 147);
+            this.textBoxBin2.Multiline = true;
+            this.textBoxBin2.Name = "textBoxBin2";
+            this.textBoxBin2.Size = new System.Drawing.Size(191, 36);
+            this.textBoxBin2.TabIndex = 9;
+            this.textBoxBin2.TextChanged += new System.EventHandler(this.textBoxBin2_TextChanged);
+            this.textBoxBin2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxBin2_KeyDown);
+            // 
+            // textBoxHex2
+            // 
+            this.textBoxHex2.Location = new System.Drawing.Point(315, 121);
+            this.textBoxHex2.Name = "textBoxHex2";
+            this.textBoxHex2.Size = new System.Drawing.Size(191, 20);
+            this.textBoxHex2.TabIndex = 8;
+            this.textBoxHex2.TextChanged += new System.EventHandler(this.textBoxHex2_TextChanged);
+            this.textBoxHex2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxHex2_KeyDown);
+            // 
+            // UmrechnerAsciiHExBin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 191);
+            this.Controls.Add(this.labelBin2);
+            this.Controls.Add(this.labelHex2);
+            this.Controls.Add(this.textBoxBin2);
+            this.Controls.Add(this.textBoxHex2);
+            this.Controls.Add(this.labelUTF8);
+            this.Controls.Add(this.textBoxUTF8);
+            this.Controls.Add(this.labelBin);
+            this.Controls.Add(this.labelHex);
+            this.Controls.Add(this.labelAscii);
+            this.Controls.Add(this.textBoxBin);
+            this.Controls.Add(this.textBoxHex);
+            this.Controls.Add(this.textBoxAscii);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "UmrechnerAsciiHExBin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Umrechner Ascii/Hex/Bin/UTF-8";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UmrechnerAsciiHExBin_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAscii;
+        private System.Windows.Forms.TextBox textBoxHex;
+        private System.Windows.Forms.TextBox textBoxBin;
+        private System.Windows.Forms.Label labelAscii;
+        private System.Windows.Forms.Label labelHex;
+        private System.Windows.Forms.Label labelBin;
+        private System.Windows.Forms.TextBox textBoxUTF8;
+        private System.Windows.Forms.Label labelUTF8;
+        private System.Windows.Forms.Label labelBin2;
+        private System.Windows.Forms.Label labelHex2;
+        private System.Windows.Forms.TextBox textBoxBin2;
+        private System.Windows.Forms.TextBox textBoxHex2;
+    }
+}
+
---
+++ AsciiBinHex/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AsciiBinHex
+{
+    public partial class UmrechnerAsciiHExBin : Form
+    {
+        List<string> BinList = new List<string>();
+        //string binListString;
+
+        bool changedByCode = false;
+
+        public UmrechnerAsciiHExBin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxAscii_TextChanged(object sender, EventArgs e)
+        {
+            //Ascii in Hex
+            StringBuilder sBuffer = new StringBuilder();
+            for (int i = 0; i < textBoxAscii.Text.Length; i++)
+            {
+                sBuffer.Append(Convert.ToInt32(textBoxAscii.Text[i]).ToString("x"));
+            }
+            textBoxHex.Text = sBuffer.ToString().ToUpper();
+
+            HexInBin();
+        }
+
+        private void textBoxHex_TextChanged(object sender, EventArgs e)
+        {
+            //System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            //byte[] dBytes = encoding.GetBytes(textBoxHex.Text);
+
+            //string utf8result = System.Text.Encoding.UTF8.GetString(dBytes);
+            //textBoxUTF8.Text = utf8result;
+
+            HexInBin();
+            BinInAscii();
+        }
+
+        private void textBoxBin_TextChanged(object sender, EventArgs e)
+        {
+            int zahlBin = 0;
+            foreach (var item in textBoxBin.Text)
+            {
+                if (item.ToString() == "0" || item.ToString() == "1")
+                {
+                    zahlBin++;
+                }
+            }
+            if (zahlBin == textBoxBin.Text.Length)
+            {
+                //Bin in Hex
+                if (textBoxAscii.Text.Length <= 8)
+                {
+                    if (textBoxBin.Text.Length % 4 == 0 && textBoxBin.Text != "")
+                    {
+                        textBoxHex.Text = Convert.ToInt64(textBoxBin.Text, 2).ToString("X");
+                    }
+
+                    if (textBoxBin.Text == "" || textBoxBin.Text == null)
+                    {
+                        textBoxHex.Text = "";
+                    }
+                }
+                BinInAscii();
+            }
+            else
+            {
+                MessageBox.Show("Ungültige Eingabe");
+                textBoxBin.Text = "";
+            }
+        }
+
+        private void HexInBin()
+        {
+            string binaercode = String.Join(String.Empty, textBoxHex.Text.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+            textBoxBin.Text = binaercode;
+        }
+
+        private void BinInAscii()
+        {
+            if (textBoxHex.Text.Length % 2 == 0)
+            {
+                string binary = textBoxBin.Text;
+                var list = new List<Byte>();
+                for (int i = 0; i < binary.Length; i += 8)
+                {
+                    if (binary.Length >= i + 8)
+                    {
+                        String t = binary.Substring(i, 8);
+                        list.Add(Convert.ToByte(t, 2));
+                    }
+                }
+                textBoxAscii.Text = Encoding.ASCII.GetString(list.ToArray());
+            }
+        }
+
+        private void textBoxUTF8_TextChanged(object sender, EventArgs e)
+        {
+            //UTF-8 in Hex
+            //Hex in Bin
+        }
+
+        private void textBoxHex2_TextChanged(object sender, EventArgs e)
+        {
+            //todo
+            if (!changedByCode)
+            {
+                HexInBin2();
+            }
+            changedByCode = false;
+
+        }
+
+        private void HexInBin2()
+        {
+            //ToDo1 -> fehler hier geht es nichtmehr raus und erhöht immmer den text von der box (nurr wenn ich das bei ToDo2 mache)
+            string binaercode = String.Join(String.Empty, textBoxHex2.Text.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
+
+            changedByCode = true;
+            textBoxBin2.Text = binaercode;
+
+
+        }
+
+        #region Methoden für UTF-8
+
+        private void HexInUTF8()
+        {
+            byte[] dBytes = StringToByteArray(textBoxHex2.Text);
+
+            string utf8result = System.Text.Encoding.UTF8.GetString(dBytes);
+            textBoxUTF8.Text = utf8result;
+        }
+
+        public static byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length / 2;
+            byte[] bytes = new byte[NumberChars];
+            using (var sr = new StringReader(hex))
+            {
+                for (int i = 0; i < NumberChars; i++)
+                    bytes[i] =
+                      Convert.ToByte(new string(new char[2] { (char)sr.Read(), (char)sr.Read() }), 16);
+            }
+            return bytes;
+        }
+
+        #endregion
+
+        private void textBoxBin2_TextChanged(object sender, EventArgs e)
+        {
+            int zahlBin2 = 0;
+            foreach (var item in textBoxBin2.Text)
+            {
+                if (item.ToString() == "0" || item.ToString() == "1")
+                {
+                    zahlBin2++;
+                }
+            }
+            if (zahlBin2 == textBoxBin2.Text.Length)
+            {
+                //BinInHex
+                //hier hängt sich das programm auf bei hex in bin, da sich der text ändert und dardurch geht es auch beim löschen in die methode (und da die binlist immer die werte sich speichert und nicht löscht gibt es fheler)-> ToDo2
+                //if (textBoxBin2.Text.Length % 4 == 0 && textBoxBin2.Text != "")
+                //{
+                //    binListString = "";
+
+                //    int startindex = textBoxBin2.Text.Length - 4;
+                //    string gekürtzterString = textBoxBin2.Text.Substring(startindex, textBoxBin2.Text.Length - startindex);
+
+                //    var hexvalue = (Convert.ToInt64(gekürtzterString, 2).ToString("X"));
+                //    BinList.Add(hexvalue);
+
+                //    foreach (var item in BinList)
+                //    {
+                //        binListString = binListString + item.ToString();
+                //    }
+
+                //    textBoxHex2.Text = binListString;
+                //}
+
+                if (textBoxBin2.Text.Length % 4 == 0 && textBoxBin2.Text != "")
+                {
+                    textBoxHex2.Text = (Convert.ToInt64(textBoxBin2.Text, 2).ToString("X")); //-> zu klein sonst funktioniert es
+                }
+
+                if (textBoxBin2.Text == "" || textBoxBin2.Text == null)
+                {
+                    textBoxHex2.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ungültige Eingabe");
+                textBoxBin2.Text = "";
+            }
+            //Hex in UTF-8
+            HexInUTF8();
+        }
+
+        #region KeyDown
+
+        private void textBoxHex2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Return)
+            {
+                changedByCode = true;
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        private void UmrechnerAsciiHExBin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        private void Beenden()
+        {
+            DialogResult result = MessageBox.Show("Wollen Sie beenden?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            { }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void textBoxAscii_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        private void textBoxHex_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        private void textBoxBin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        private void textBoxUTF8_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        private void textBoxBin2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Beenden();
+            }
+        }
+
+        #endregion
+    }
+}
